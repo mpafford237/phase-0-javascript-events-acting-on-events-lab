05144821f60492d98d4eceee7ea80b30559aa112
Mason Pafford <mdpafford2015@gmail.com>
2023-11-07 20:14:29 -0600

    wow almost done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    left -= 1;
+    dodger.style.left = `${left}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    left += 1;
+    dodger.style.left = `${left}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
